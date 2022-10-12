ea92e6c680abc48cab5511ac5ca5d928f60b0d3a
EvanHyett <evan1hyett@gmail.com>
2022-10-12 10:57:56 -0700

    still have errors => needs work

--- sum_of_squares/src/main.rs
+++ sum_of_squares/src/main.rs
@@ -1,11 +1,35 @@
-use std::ops::Mul;
+use std::cmp::Ord;
+use std::ops::{Add, Mul};
 
-// Square of a given number
-fn square<T>(x: T) -> T
-where 
+/// Square of a given number
+
+fn main() {
+    fn square<T>(x: T) -> T
+where
     T: Copy + Mul<Output = T>,
 {
     x * x
+}
+
+/// Sum of squares of two inputs
+fn sum_of_squares<T>(x: T, y: T) -> T
+where
+    T: Copy + Add<Output = T> + Mul<Output = T>,
+{
+    square(x) + square(y)
+}
+
+pub fn sum_of_largest_two_squares<T>(x: T, y: T, z: T) -> T
+where
+    T: Copy + Ord + Add<Output = T> + Mul<Output = T>,
+{
+    if x >= z && y >= z {
+        sum_of_squares(x, y)
+    } else if y >= x && z >= x {
+        sum_of_squares(y, z)
+    } else {
+        sum_of_squares(x, z)
+    }
 }
 
 #[cfg(test)]
@@ -14,10 +38,25 @@ mod tests {
 
     #[test]
     fn test_square() {
-        assert_eq!(square(2), 4)
+        assert_eq!(square(2), 4);
+    }
+
+    #[test]
+    fn test_sum_of_squares() {
+        assert_eq!(sum_of_squares(1, 2), 5);
+        assert_eq!(sum_of_squares(2, 3), 13);
+    }
+
+    #[test]
+    fn test_sum_of_largest_two_squares() {
+        let sum = sum_of_largest_two_squares(1, 2, 3);
+        assert_eq!(sum, 13);
+        let sum = sum_of_largest_two_squares(1, 1, 1);
+        assert_eq!(sum, 2);
+        let sum = sum_of_largest_two_squares(1, 2, 2);
+        assert_eq!(sum, 8);
+        let sum = sum_of_largest_two_squares(1, 1, 2);
+        assert_eq!(sum, 5);
     }
 }
-
-fn main() {
-    println!("Hello, world!");
 }
