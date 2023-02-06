65282df77dc6cebd9ef0a2c37af2752617aabb36
EvanHyett <evan1hyett@gmail.com>
2023-02-06 09:28:54 -0800

    commit tuple practice

--- practice_proj/src/main.rs
+++ practice_proj/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    // variable tup binds to the entire tuple
+    // To get individual values out of a tuple, we can use pattern
+    // matching to destructure a tuple value like this:
+    let (x, y, z) = tup;
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
 }
