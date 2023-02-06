6efb2106a2b99411e5e9212ff58bc157439079d4
EvanHyett <evan1hyett@gmail.com>
2023-02-06 08:59:39 -0800

    commit

---
+++ practice_proj/target/debug/deps/practice_proj.d
@@ -0,0 +1,5 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\practice_proj\target\debug\deps\practice_proj.exe: src\main.rs
+
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\practice_proj\target\debug\deps\practice_proj.d: src\main.rs
+
+src\main.rs:
---
+++ practice_proj/target/debug/practice_proj.d
@@ -0,0 +1,1 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\practice_proj\target\debug\practice_proj.exe: C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\practice_proj\src\main.rs
