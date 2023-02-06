53e6b8bb9b7cfddbecdbd914cd36271b6edf77b9
EvanHyett <evan1hyett@gmail.com>
2023-02-06 10:08:42 -0800

    commit array practice

--- array-prac/src/main.rs
+++ array-prac/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    println!("{:?}", months);
 }
