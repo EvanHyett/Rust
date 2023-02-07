a4ef315c9348aa174948c27297b529b5ad9bd428
EvanHyett <evan1hyett@gmail.com>
2023-02-07 13:41:16 -0800

    practice

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\Users\evan1\OneDrive\Documents\github\rust-prac\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+C:\Users\evan1\OneDrive\Documents\github\rust-prac\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\evan1\OneDrive\Documents\github\rust-prac\rectangles\target\debug\rectangles.exe: C:\Users\evan1\OneDrive\Documents\github\rust-prac\rectangles\src\main.rs
