c44b86ebc66e191460c2a1bd92b109678b3477bf
EvanHyett <evan1hyett@gmail.com>
2023-02-10 20:46:26 -0800

    meh

---
+++ enums/target/debug/deps/enums.d
@@ -0,0 +1,5 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\enums\target\debug\deps\enums.exe: src\main.rs
+
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\enums\target\debug\deps\enums.d: src\main.rs
+
+src\main.rs:
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\enums\target\debug\enums.exe: C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\enums\src\main.rs
