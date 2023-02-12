8cc987e7b7b30ef66670bc8016a9260c0490985f
EvanHyett <evan1hyett@gmail.com>
2023-02-12 08:27:22 -0800

    modules practice commit

---
+++ modules_prac/target/debug/deps/modules_prac.d
@@ -0,0 +1,5 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\modules_prac\target\debug\deps\modules_prac.exe: src\main.rs
+
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\modules_prac\target\debug\deps\modules_prac.d: src\main.rs
+
+src\main.rs:
---
+++ modules_prac/target/debug/modules_prac.d
@@ -0,0 +1,1 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\modules_prac\target\debug\modules_prac.exe: C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\modules_prac\src\main.rs
