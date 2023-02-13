70c321460c6053b59f5c77a71711a8e49b9dab33
EvanHyett <evan1hyett@gmail.com>
2023-02-13 09:33:12 -0800

    practicing vectors

---
+++ vectors_prac/target/debug/deps/vectors_prac.d
@@ -0,0 +1,5 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\vectors_prac\target\debug\deps\vectors_prac.exe: src\main.rs
+
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\vectors_prac\target\debug\deps\vectors_prac.d: src\main.rs
+
+src\main.rs:
---
+++ vectors_prac/target/debug/vectors_prac.d
@@ -0,0 +1,1 @@
+C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\vectors_prac\target\debug\vectors_prac.exe: C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\vectors_prac\src\main.rs
