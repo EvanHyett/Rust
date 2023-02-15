e87806afa825984a086bf38a88d6c9fe36f371cc
EvanHyett <evan1hyett@gmail.com>
2023-02-15 08:49:03 -0800

    commit

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, mafucka");
+
+    println!("Please choose a number");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    println!("The number you typed was: {}", input);
+
 }
+
+     //     0
+     //    /|\
+     //    | | 
--- hangman/target/debug/deps/hangman.d
+++ hangman/target/debug/deps/hangman.d
@@ -1,5 +1,5 @@
-C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\hangman\target\debug\deps\hangman.exe: src\main.rs
+C:\users\evan1\onedrive\documents\github\rust-prac\hangman\target\debug\deps\hangman.exe: src\main.rs
 
-C:\Users\evan1\OneDrive\Documents\GitHub\rust-prac\hangman\target\debug\deps\hangman.d: src\main.rs
+C:\users\evan1\onedrive\documents\github\rust-prac\hangman\target\debug\deps\hangman.d: src\main.rs
 
 src\main.rs:
